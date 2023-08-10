7182b627ea89c2e138ecef78e841809b3a41d506
Ivan Palmar <palmar.ivan0205@gmail.com>
2023-08-09 20:53:44 -0500

    Correcciones(Busca, navegabilidad)-IvanPalmar

--- hospitalProyecto/JS/index.js
+++ hospitalProyecto/JS/index.js
@@ -1,50 +1,69 @@
 function buscar(){
-    var enlaces = document.querySelectorAll('.barraBuscar a');
 
     var busqueda = document.getElementById("barra");
 
-    var lionsGateHospital = 'Lions Gate Hospital'
+    var enlaces = document.querySelectorAll('.barraBuscar a');
 
+    if(busqueda.value == 'Descubre Lions' || busqueda.value == 'descrubir lions' || busqueda.value == 'descripcion' || busqueda.value == 'Descripcion'
+        || busqueda.value == 'Descrubir Lions' || busqueda.value == "Sobre nosotros" || busqueda.value == "Sobre Nosotros" || busqueda.value == "Bibliografia"){
 
-    if(busqueda.value == 'Registrarse' || busqueda.value == 'Registro' || busqueda.value == 'registro' || busqueda.value == 'registrarse'
-        || busqueda.value == 'Realizar registro'){
+            document.getElementById("Descripcion").style.display="block";
 
-            enlaces.forEach(function(element){
-
-                element.href = '/pages/registrarse.html'
-                
-            });
-
-        return true
-    }
-
-    else if(busqueda.value == 'Iniciar Sesion' || busqueda.value == 'iniciar sesion' || busqueda.value == 'Iniciar sesion' || 
-        busqueda.value=='iniciar Sesion' || busqueda.value == 'inicio de sesion' || busqueda.value == 'Inicio de Sesion'){
-
-            enlaces.forEach(function(element){
-
-                element.href = "/pages/iniciarSesion.html"
-
-            })
-
-        return true
-
-    }
-
-    else if(busqueda.value){
-        enlaces.forEach(function (element) {
-
-            element.href = `https://www.google.com/search?q=${lionsGateHospital+" "+busqueda.value}`;
+            document.getElementById("serviciosIncluidos").style.display = "none";
         
-        });
+            document.getElementById("Recursos").style.display = "none";
+
+            busqueda.value = "";
+
         return true
     }
-    else{
-        enlaces.forEach(function (element) {
 
-        element.href = "#";
-        
-        });     
-        return false
+    else if(busqueda.value == 'Servicios Lions' || busqueda.value == 'Servicios' || busqueda.value == 'Servicios Incluidos' || 
+        busqueda.value=='servicios lions' || busqueda.value == 'servicios' || busqueda.value == 'servicios incluidos' || busqueda.value == "Servicios incluidos"){
+
+            document.getElementById("Descripcion").style.display="none";
+
+            document.getElementById("serviciosIncluidos").style.display = "block";
+
+            document.getElementById("Recursos").style.display = "none";
+
+            document.getElementById("serviciosIncluidos").style.marginTop="-80px";
+
+            busqueda.value = "";
+
+        return true
+
     }
+
+    else if(busqueda.value == "Recursos Lions" || busqueda.value == "recursos" || busqueda.value == "recursos lions" || busqueda.value == "Recursos"){
+
+        document.getElementById("Descripcion").style.display="none";
+
+        document.getElementById("serviciosIncluidos").style.display = "none";
+
+        document.getElementById("Recursos").style.display = "block";
+
+        document.getElementById("Recursos").style.marginTop="-30px";
+
+        busqueda.value = "";
+
+        return true
+    }
+
+    else{            
+            document.getElementById("Descripcion").style.display="block";
+
+            document.getElementById("serviciosIncluidos").style.display = "block";
+
+            document.getElementById("Recursos").style.display = "block";
+
+            document.getElementById("serviciosIncluidos").style.marginTop="0px";
+
+            document.getElementById("Recursos").style.marginTop="0px";
+
+            busqueda.value = "";
+
+        return true
+    }
+
 }
