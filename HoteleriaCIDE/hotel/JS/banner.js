964422c4a4bdcbce680f039f2d83f7bd90514860
Ivan Palmar <palmar.ivan0205@gmail.com>
2023-09-22 22:47:59 -0500

    Proyeto Final ZIP - BackUp Reto CIDE

---
+++ HoteleriaCIDE/hotel/JS/banner.js
@@ -0,0 +1,50 @@
+var contenedor = $('#caja');
+
+//La funcion $() es una abreviatura igual a document.GetElementById()
+
+var siguiente = $('#btn-adelante');
+
+var anterior = $('#btn-atras');
+
+$('#caja .section_caja:last').insertBefore
+('#caja .section_caja:first');
+
+contenedor.css('margin-left','-'+100+'%');
+
+function derecha(){
+    contenedor.animate({
+        marginLeft:'-'+200+'%'
+    },700,function(){
+        $('#caja .section_caja:first').insertAfter('#caja .sectiion_caja:last');
+        contenedor.css('margin-left','-'+100+'%');
+    });
+};
+
+function izquierda(){
+    contenedor.animate({
+        marginLeft:0
+    },700,function(){
+        $('#caja .section_caja:first').insertBefore('#caja .sectiion_caja:last');
+        contenedor.css('margin-left','-'+100+'%');
+    });
+};
+
+function automatico(){
+    interval = setInterval(function(){
+        derecha();
+    },5000);
+};
+
+siguiente.on("click", function(){
+    derecha();
+    clearInterval(interval);
+    automatico();
+});
+
+anterior.on("click", function(){
+    izquierda();
+    clearInterval(interval);
+    automatico();
+});
+
+automatico();
